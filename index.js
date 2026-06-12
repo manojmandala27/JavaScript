966a0ce6fdc43b789935bb58ad8058d93e172c0d
manoj <manojmandala07@gmail.com>
2026-06-12 13:58:55 +0530

    added successfully

---
+++ index.js
@@ -0,0 +1,42 @@
+
+//var a=10
+//var b=27.2
+//var c="hello"
+//var d=false
+
+//console.log(a+" "+b+" "+c+" "+d)
+
+var a=10
+var b="10"
+if(a==b) {
+    console.log("equals");
+} 
+else {
+    console.log("not equals");
+}
+
+if(a===b) {
+    console.log("equals")
+}
+ else {
+    console.log("not equals")
+}
+
+console.log(2*3)
+console.log(2**3)
+
+
+for(var i=0;i<10;i++)
+{
+    console.log(i+" ")
+}
+
+var n=2000
+if((n % 4 == 0 && n % 100 !=0)|| (n % 400 ==0)) {
+    console.log("leap year")
+}
+else
+{
+    console.log("Not a leap year")
+}
+
